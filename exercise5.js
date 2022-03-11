34f13b160bab916b9406dd7f4b144347540b54e9
EagleCollin <scoutcollin97@gmail.com>
2022-03-10 18:47:46 -0600

    Finished Exercise 5

--- exercise5.js
+++ exercise5.js
@@ -1,4 +1,7 @@
-let toArray = {me: 1, her: 2, you: 3, him: 4 }
+const toArray = {me: 1, her: 2, you: 3, him: 4 }
 
 
-const myArr= new Array()
+
+
+const myArr = Object.entries(toArray)
+console.log(myArr)
